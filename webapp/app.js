436c482624c7411b1200947a7bf4e276f00aee87
Jason Tan <jtan20040401@gmail.com>
2023-06-20 23:59:19 +1000

    Updated

--- webapp/app.js
+++ webapp/app.js
@@ -11,7 +11,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/test', function (req, res) {
-    res.send('the REST endpoint test run!');
+    res.send('the REST endpoint test run updated!');
 });
 
 app.listen(port, function() {
